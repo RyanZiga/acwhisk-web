25146fa4d82dcd77c340552f3f6ad09b8680b1e6
RyanZiga <87555605+RyanZiga@users.noreply.github.com>
2025-08-31 14:19:01 +0800

    Add files via upload

---
+++ pages/maintenance.tsx
@@ -0,0 +1,9 @@
+// pages/maintenance.tsx
+export default function MaintenancePage() {
+  return (
+    <div style={{ textAlign: 'center', paddingTop: '10rem' }}>
+      <h1>🚧 Site Under Maintenance</h1>
+      <p>We're currently making updates. Please check back soon!</p>
+    </div>
+  );
+}
